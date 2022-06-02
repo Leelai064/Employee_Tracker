571819246723df0aea949c5c5ce26f5754e2617b
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-06-01 21:23:43 -0400

    package.json added. schema and database needed to finish connect.js

---
+++ connect/connect.js
@@ -0,0 +1,23 @@
+// my sql installation
+const mysql = require('mysql');
+
+// Found a setup guide that helped me in W3schools
+module.exports = connect;
+connection = mysql.createConnection({
+    
+    host:'localhost',
+    user:"root",
+    password: 'rootroot',
+    database: '',
+    mutipleStatements:true
+
+});
+
+confirm.connect(function(err){
+    if (err){
+        console.log((err));
+        return;
+    }
+
+    console.log()
+});
